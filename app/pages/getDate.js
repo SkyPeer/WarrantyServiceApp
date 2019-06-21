ea4a4187a84aead8beb10eb35184e92674d90a63
Oleg Selivantsev <o.selivancev@gmail.com>
2019-06-21 22:21:37 +0300

    Update getDate.js

--- app/pages/getDate.js
+++ app/pages/getDate.js
@@ -1,6 +1,5 @@
 module.exports = (_currentDate, _ticketDate, _daysLeft) => {
 
-<<<<<<< HEAD
     const dateOfCreation = new Date(_ticketDate);
     const finishDate = new Date(_ticketDate);
     const currentDate = new Date(_currentDate);
@@ -28,34 +27,4 @@ module.exports = (_currentDate, _ticketDate, _daysLeft) => {
         daysLeftLocal: daysLeftLocal < 0 ? 'просрочено: ' + daysLeftLocal * -1 + ' ' : daysLeftLocal + '',
         daysLeftClass: daysLeftClass
     }
-=======
-        const dateOfCreation = new Date(_ticketDate);
-        const finishDate = new Date(_ticketDate);
-        const currentDate = new Date(_currentDate);
-        const minutes = dateOfCreation.getMinutes() < 10 ? '0'+dateOfCreation.getMinutes(): dateOfCreation.getMinutes();
-        let daysLeftClass = '';
-
-        finishDate.setDate(dateOfCreation.getDate()+parseInt(_daysLeft));
-
-        const daysLeftLocal = Math.round((finishDate - currentDate) / 1000 / 60 / 60/ 24);
-
-
-
-
-        switch (true) {
-            case(daysLeftLocal < 5)  :
-                daysLeftClass = 'daysForService_red';
-                break;
-            case(daysLeftLocal < 10)  :
-                daysLeftClass = 'daysForService_yellow';
-                break;
-            default:
-                daysLeftClass = 'daysForService_green';
-        }
-
-
-        return {dateOfCreation: (dateOfCreation.getDate() + '/' + (dateOfCreation.getMonth()+parseInt(1)) + '/' +dateOfCreation.getFullYear()+' '+dateOfCreation.getHours()+':'+ minutes ),
-            finishDate: (finishDate.getDate() + '/' +(finishDate.getMonth()+parseInt(1)) + '/' +finishDate.getFullYear()),
-            daysLeftLocal: daysLeftLocal, daysLeftClass: daysLeftClass}
->>>>>>> parent of 6acbee8... getDate update
 };
